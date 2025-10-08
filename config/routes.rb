2bc0c5f531b96e8decdb2600c1095f0e13594573
susuki-m <212071147+susuki-m@users.noreply.github.com>
2025-10-08 16:18:56 +0000

    [add]投稿画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show]
+  
+  resources :post_images, only: [:new, :create, :index, :show]
   devise_for :users
   get 'homes/about', to: 'homes#about', as: 'about'
   root to: "homes#top"
